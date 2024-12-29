05093447c3f54b2cd753ecfefbb3e228d3450635
SKC9372 <suryakantchaubey2001@gmail.com>
2024-12-29 15:07:27 +0530

    requirements installed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Chest Cancer Prediction',
+    version='0.0.0',
+    author_email='suryakantchaubey2001@gmail.com',
+    author='Suryakantchaubey',
+    packages=find_packages(),
+    install_requires=['antlr4-python3-runtime']
+)
